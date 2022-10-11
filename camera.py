e539ae15cdc1e18da9a2ee20bedc65c53186f909
pcruiher08 <pcruiher089@gmail.com>
2022-10-11 09:36:05 -0500

    taking pictures and weight

--- camera.py
+++ camera.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 import sys
 import serial
-
+import fnmatch
 import gphoto2 as gp
 
 weight = 0
@@ -19,13 +19,16 @@ camera = gp.Camera()
 camera.init()
 print('Capturing image')
 file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
+
 #data = ser.readline()
 data = 0
 print('Weight of fruit: {0}'.format(data))
 
-
+filename = '/media/pcruiher08/phi/dataset/pictures'
+photoname = "testname" + str(len(fnmatch.filter(os.listdir(filename), '*.*')) + 1) + ".jpg"
 print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
-target = os.path.join('/tmp', file_path.name)
+target = os.path.join('/media/pcruiher08/phi/dataset/pictures', photoname)
+print('there are {0} files in the folder'.format(len(fnmatch.filter(os.listdir(filename), '*.*'))))
 print('Copying image to', target)
 camera_file = camera.file_get(
     file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
